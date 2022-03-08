3b4ffb4668ca92deb380a97b026d80658593b1ea
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-03-08 22:16:42 +0400

    Tasklar hazirdi

---
+++ Task-c/Task-c/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task_c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Eight();
+        }
+        public static int Three()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i += 3)
+            {
+                if (number < 50)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            return number;
+        }
+        public static int Five()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i += 5)
+            {
+                if (number > 50 && number < 100)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            return number;
+        }
+        public static int Eight()
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i += 8)
+            {
+                if (number > 100)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            return number;
+        }
+    }
+}
